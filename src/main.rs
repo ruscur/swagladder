271070c78f5b5fb80d98097583924b83f11eff04
Russell Currey <ruscur@russell.cc>
2017-01-24 14:11:29 +1100

    Quick redis test before I go too ham
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate iron;
 extern crate rustc_serialize;
+extern crate redis;
 
 mod elo;
 mod player;
@@ -17,6 +18,13 @@ fn main() {
     let rating_system = EloRanking::new(32);
     let mut ruscur = Player::new("ruscur".to_string());
     let mut njd = Player::new("Nick".to_string());
+
+    // test redis
+    let client = try!(redis::Client::open("redis://127.0.0.1/"));
+    let con = try!(client.get_connection());
+    let _ : () = try!(con.set("test", 420));
+
+    println!("{}", try!(con.get("my_key")));
 
 
     rating_system.win::<Player>(&mut ruscur, &mut njd);
